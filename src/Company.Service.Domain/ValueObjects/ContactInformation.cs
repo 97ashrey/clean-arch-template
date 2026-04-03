75ebbd8cd1db96b6efa8ed3b3db679398209f0d1
97ashrey <97a.kojic@gmail.com>
2026-04-04 00:12:25 +0200

    Moved ContactInformation to ValueObjects

---
+++ src/Company.Service.Domain/ValueObjects/ContactInformation.cs
@@ -0,0 +1,29 @@
+using Company.Service.Domain.Common.Types;
+using Company.Service.Domain.Common.Types.Errors;
+
+public record ContactInformation()
+{
+    public string FirstName { get; private set; } = string.Empty;
+    public string LastName { get; private set; } = string.Empty;
+    public string Email { get; private set; } = string.Empty;
+    public string PhoneNumber { get; private set; } = string.Empty;
+
+    private ContactInformation(string firstName, string lastName, string email, string phoneNumber) : this()
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    public static Result<ContactInformation, ValidationError> CreateNew(
+        string firstName, string lastName, string email, string phoneNumber)
+    {
+        return Validate.ExecuteRules(
+            Validate.NotEmpty(firstName, nameof(firstName)),
+            Validate.NotEmpty(lastName, nameof(lastName)),
+            Validate.NotEmpty(email, nameof(email)),
+            Validate.NotEmpty(phoneNumber, nameof(phoneNumber))
+        ).Map(() => new ContactInformation(firstName, lastName, email, phoneNumber));
+    }
+}
